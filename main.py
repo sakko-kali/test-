cc544fee4eafd8f275f7f9418a26fb00e1e4c8fc
Sak <sarkisssbarsegain@gmail.com>
2024-12-02 02:38:12 +0400

    third sommit

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def square(a):
     return a**2
 
 print(square(5))
+
+def listsum(*args):
+    sum = 0
+    for i in args:
+        sum +=i
+    return sum
+
+print(listsum(5,3,4,23,4342,324,214,3))
+
